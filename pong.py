e6273fd74e60c2231270908d4439e27975e25617
lemoustachu <lemoustrashu@outlook.com>
2021-04-07 11:21:52 +0200

    ajout d'une fenetre et creation des variables de couleur

--- pong.py
+++ pong.py
@@ -1,1 +1,12 @@
+#on importe pygame et on l'initialise
+import pygame
+pygame.init()
 
+#on definit les couleurs utilisées(blanc et noir)
+NOIR = (0,0,0)
+BLANC = (255,255,255)
+
+#on ouvre une fenetre
+taille_de_la_fenetre = (400, 300)
+ecran = pygame.display.set_mode(taille_de_la_fenetre)
+pygame.display.set_caption("Pong")
