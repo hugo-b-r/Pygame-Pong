f31950ee42e52f4d3347ca331c390c3f0cc0be9c
lemoustachu <lemoustrashu@outlook.com>
2021-05-17 19:39:49 +0200

    changing the name of the program and restart it from 0

---
+++ main.py
@@ -0,0 +1,34 @@
+import pygame as pyg
+import sys
+
+class Game:
+
+    def __init__(self):
+        
+        #we define here resolution
+        self.display = pyg.display.set_mode((700, 500))
+
+        #we put here the caption
+        pyg.dislpay.set_caption('Pong Game')
+
+        #we set the game to true
+        self.is_the_game_running = True
+
+    def main_loop(self):
+
+        while self.is_the_game_running:
+            #test for evnts
+            for event in pyg.even.get():
+
+                if event.type == pyg.QUIT():
+
+                    sys.exit()
+
+            self.display.fill((200, 200, 200))
+            pyg.display.flip()
+
+if __name__ == '__main__':
+
+    pyg.init()
+    Game().main_loop()
+    pyg.quit()
