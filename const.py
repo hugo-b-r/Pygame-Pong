dfd45f12f364a8dece7132355ccc0698aacab72f
lemoustachu <lemoustrashu@outlook.com>
2021-05-19 08:56:30 +0200

    we have the paddles/players that can move but you have to release the key to move far

--- const.py
+++ const.py
@@ -1,7 +1,8 @@
-#colours
-WHITE = (200, 200, 200)
-BLACK = (0, 0, 0)
+import pygame
 
+#colours
+WHITE = pygame.Color(200, 200, 200)
+BLACK = pygame.Color(0,0,0)
 #sizes
 bat_height = 80
 bat_width = 20
--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 import pygame as pygame
 import sys
 import const
+import time
 
 from pygame.constants import K_UP
+
+#INITIALIZATION OF THE SCREEN
+
+#we define here resolution
+DISPLAY = pygame.display.set_mode((720, 480))
+
+#we put here the caption
+pygame.display.set_caption('Pong Game')
+
+is_a_game_running = True
+
+#the display
+DISPLAY.fill(const.BLACK)
+pygame.display.flip()
 
 #player class
 class Player:
@@ -15,71 +30,64 @@ class Player:
 
 
     #how we show the bat
-    def show(self, color):
+    def show(self, color,):
         
-        pygame.draw.rect(the_game.display, color, (self.x, self.y, const.bat_width, const.bat_height))
+        pygame.draw.rect(DISPLAY, color, (self.x, self.y, const.bat_width, const.bat_height))
+
+        pygame.display.update()
 
     #how the player move
     def move_up(self):
         self.show(const.BLACK)
-        self.x += -10
+        self.y += -20
         self.show(const.WHITE)
 
     def move_down(self):
         self.show(const.BLACK)
-        self.x += 1010
+        self.y += 20
         self.show(const.WHITE)
 
+  
+#init players
+#player 1
+Player_1 = Player(40, 240)
+ 
+#player 2
+Player_2 = Player(660, 240)
 
-class Game:
 
-    def __init__(self):
-        
-        #we define here resolution
-        self.display = pygame.display.set_mode((700, 500))
-
-        #we put here the caption
-        pygame.display.set_caption('Pong Game')
-
-        #we set the game to true
-        self.is_the_game_running = True
-
-        #init players
-        #player 1
-        Player_1 = Player(20, 250)
-        Player_1.show(const.WHITE)
-
-        #player 2
-        Player_2 = Player(860, 250)
-        Player_2.show(const.WHITE)
-
-    def main_loop(self):
-
-        while self.is_the_game_running:
-            #test for evnts
-            for event in pygame.event.get():
-
-                if event.type == pygame.QUIT:
-
-                    sys.exit()
-
-                if event.type == pygame.KEYDOWN:
-                    #for player 2
-                    if event.key == pygame.K_UP:
-                        self.speed_2_y = -10
-
-                    if event.key == pygame.K_DOWN:
-                        self.speed_2_y = 10
+def show_players():
+    Player_1.show(const.WHITE)
+    Player_2.show(const.WHITE)
 
 
 
-            self.display.fill(const.BLACK)
-            pygame.display.flip()
+def main_loop():
+    show_players()
+    is_a_game_running = True
+    while is_a_game_running:
+            #test for events
+        for event in pygame.event.get():
 
-if __name__ == '__main__':
+            if event.type == pygame.QUIT:
+                is_a_game_running = False
+                sys.exit()
 
-    pygame.init()
-    the_game = Game
+            if event.type == pygame.KEYDOWN:
+                #for player 2
+                if event.key == pygame.K_UP:
+                    Player_2.move_up()
+                    
 
-    the_game.main_loop()
-    pygame.quit()
+                if event.key == pygame.K_DOWN:
+                    Player_2.move_down()
+
+
+pygame.init()
+
+
+#new game
+main_loop()
+
+
+pygame.quit()
