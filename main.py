292bb0567e766343663cbde087e1b919ca0ad094
lemoustachu <lemoustrashu@outlook.com>
2021-05-19 11:18:28 +0200

    i addd a test file and the paddle 2 can move and can't move  up out but out down is POSSIBLE

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import sys
 import const
 import time
 
-from pygame.constants import K_UP
+from pygame.constants import K_DOWN, K_UP
 
+
+#------------------------------------------------------------------------------------------------------
 #INITIALIZATION OF THE SCREEN
 
 #we define here resolution
@@ -14,46 +16,61 @@ DISPLAY = pygame.display.set_mode((720, 480))
 pygame.display.set_caption('Pong Game')
 
 is_a_game_running = True
-
+clock = pygame.time.Clock()
 #the display
 DISPLAY.fill(const.BLACK)
 pygame.display.flip()
+
+
+
+#------------------------------------------------------------------------------------------------------
+
 
 #player class
 class Player:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+        self.speed = 0
         self.height = const.bat_height
         self.width = const.bat_height
 
 
 
     #how we show the bat
-    def show(self, color,):
+    def show(self, color):
         
         pygame.draw.rect(DISPLAY, color, (self.x, self.y, const.bat_width, const.bat_height))
 
         pygame.display.update()
 
     #how the player move
-    def move_up(self):
-        self.show(const.BLACK)
-        self.y += -20
-        self.show(const.WHITE)
+    def move(self):
+        print(self.y)
+        if self.speed < 0:
+            if (self.y != 0):
 
-    def move_down(self):
-        self.show(const.BLACK)
-        self.y += 20
-        self.show(const.WHITE)
+                self.show(const.BLACK)
+        
+                self.y += self.speed
+
+                self.show(const.WHITE)
+        else:
+            self.show(const.BLACK)
+        
+            self.y += self.speed
+
+            self.show(const.WHITE)
+
+#------------------------------------------------------------------------------------------------------
 
   
 #init players
 #player 1
-Player_1 = Player(40, 240)
+Player_1 = Player(40, 200)
  
 #player 2
-Player_2 = Player(660, 240)
+Player_2 = Player(660, 200)
 
 
 def show_players():
@@ -62,25 +79,51 @@ def show_players():
 
 
 
+#------------------------------------------------------------------------------------------------------
+
+
 def main_loop():
     show_players()
     is_a_game_running = True
     while is_a_game_running:
-            #test for events
-        for event in pygame.event.get():
+        
+        time = clock.tick(60)	
+        
+        if Player_1.speed != 0:
+            Player_1.move()
+        
+        if Player_2.speed != 0:
+            Player_2.move()
+        
+        
 
+        #test for events
+        keys = pygame.key.get_pressed()
+
+        if keys[K_UP] == 1:
+                Player_2.speed = -5
+
+        elif keys[K_DOWN] == 1:
+                Player_2.speed = 5
+            
+        else:
+            Player_2.speed = 0
+            
+        for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 is_a_game_running = False
                 sys.exit()
 
-            if event.type == pygame.KEYDOWN:
-                #for player 2
-                if event.key == pygame.K_UP:
-                    Player_2.move_up()
-                    
+         
+           
 
-                if event.key == pygame.K_DOWN:
-                    Player_2.move_down()
+            
+        
+
+
+
+#------------------------------------------------------------------------------------------------------
+
 
 
 pygame.init()
---
+++ test.py
@@ -0,0 +1,7 @@
+# ceci est un fichier de test
+
+
+a = -2
+b = 2
+c= a + b
+print(c)
